4beec1b2da33ef732c4e500a6e130a01338fe53e
ChikaraK <nowano0403@gmail.com>
2019-04-20 05:54:06 +0000

    DBへファイル名と、パスを保存するよう変更

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,7 +1,23 @@
 class ImagesController < ApplicationController
+	include ImagesHelper
 	def index
+		@images = Image.all
 	end
 
 	def input
+		@image = Image.new
+	end
+
+	def download
+		search_word=URI.encode(params[:search_word])
+		doc = Nokogiri::HTML(open(
+			"https://www.flickr.com/search/?q=#{search_word}"))
+		doc.xpath("//div[@class='view photo-list-photo-view requiredToShowOnServer awake']").each do |link|
+			url = link.to_s.match(/https.*?jpg|png|jpeg/)
+			#サムネ
+			save_image(url[0])
+			sleep(1)
+		end
+		redirect_to images_path
 	end
 end
--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,2 +1,16 @@
 module ImagesHelper
+	require 'open-uri'
+	require 'nokogiri'
+	def save_image(url)
+		filename = File.basename(url)
+		dirName = Rails.root.join('app', 'assets','images/').to_s
+		filepath = dirName + filename
+		open(filepath,'wb') do |file|
+			open(url) do |data|
+				file.write(data.read)
+			end
+		end
+		feed = Image.new(:name  => filename,:data => filepath)
+		feed.save
+	end
 end
